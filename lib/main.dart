ccccd8c4b7fe6b1924d0f2430d885aba16dca87c
neno666 <mohamedahmedemad2003@gmail.com>
2022-12-21 21:45:49 +0300

    create main page

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? Key}) : super(Key: Key);
+
+  @Override 
+  Wighet @override
+  Widget build(BuildContext context) {
+    return const MaterialApp( 
+      home: testttttt(),
+      );
+  }
+}
