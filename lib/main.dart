0b5b6e536ef7a4b4d56a0528fa840ecd55d612fa
neno666 <mohamedahmedemad2003@gmail.com>
2022-12-21 22:25:24 +0300

    main2

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import "package:flutter/material.dart";
+import 'package:mohamed_emad211060035/pages/login.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? Key}) : super(Key: Key);
+  const MyApp({Key? Key}) : super(key: Key);
 
-  @Override 
-  Wighet @override
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp( 
-      home: testttttt(),
-      );
+    return const MaterialApp(
+      home: Login(),
+    );
   }
 }
